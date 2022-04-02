d3d8d66776af2b768a0c73a7dcd2561d413a73f9
vbasto-iscte <vbasto@vitors-mbp>
2022-04-02 19:56:22 +0100

    .

--- src/main/java/test/WelcomeController.java
+++ src/main/java/test/WelcomeController.java
@@ -8,6 +8,6 @@ public class WelcomeController {
 
 	@GetMapping("/welcome")
 	public String welcome() {
-		return "Welcome to springboot heroku test ...!!!";
+		return "Welcome to springboot heroku test...";
 	}
 }
